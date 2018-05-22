cb56944a70d0fc652d605866dc786d7a9078b8aa
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-05-22 13:29:44 +0300

    cut down the repro

--- SlowTypecheckRepro/Program.fs
+++ SlowTypecheckRepro/Program.fs
@@ -1,35 +1,15 @@
-﻿open System.Data.SqlClient
+﻿open Microsoft.SqlServer.TransactSql.ScriptDom
 
-let rewriteSqlStatementToEnableMoreThanOneParameterDeclaration() =
-        
-    let parser = Microsoft.SqlServer.TransactSql.ScriptDom.TSql140Parser(true)
+let f() =
     let tsqlReader = new System.IO.StringReader("")
     let errors = ref Unchecked.defaultof<_>
-    let fragment = parser.Parse(tsqlReader, errors)
-
-    let allVars = ResizeArray()
-    let declaredVars = ResizeArray()
-
+    let fragment = TSql140Parser(true).Parse(tsqlReader, errors)
+    
     fragment.Accept {
-        new Microsoft.SqlServer.TransactSql.ScriptDom.TSqlFragmentVisitor() with
-            member __.Visit(node : Microsoft.SqlServer.TransactSql.ScriptDom.VariableReference) = 
-                base.Visit node
-                allVars.Add(node.Name, node.StartOffset, node.FragmentLength)
-            member __.Visit(node : Microsoft.SqlServer.TransactSql.ScriptDom.DeclareVariableElement) = 
-                base.Visit node
-                declaredVars.Add(node.VariableName.Value)
+        new TSqlFragmentVisitor() with
+            member __.Visit(node : VariableReference) = base.Visit node
+            member __.Visit(node : DeclareVariableElement) = base.Visit node
     }
-    let unboundVars = 
-        allVars 
-        |> Seq.groupBy (fun (name, _, _)  -> name)
-        |> Seq.choose (fun (name, xs) -> 
-            if declaredVars.Contains name 
-            then None 
-            else Some(name, xs |> Seq.mapi (fun i (_, start, length) -> sprintf "%s%i" name i, start, length)) 
-        )
-        |> dict
-
-    unboundVars, !errors
 
 [<EntryPoint>]
 let main _ = 
